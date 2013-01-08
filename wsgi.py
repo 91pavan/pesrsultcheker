876bb953888e658dd3e32273aeb489b41c8a3a1d
Pavan Sudheendra <pavan0591@gmail.com>
2013-01-08 17:20:21 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-
 import urllib2
 import smtplib
 import time
@@ -8,18 +7,18 @@ from apscheduler.scheduler import Scheduler
 
 def function1():
     try:
-        f = urllib2.urlopen('http://www.pes.edu/results/7-sem')
+        f = urllib2.urlopen('http://www.example.com')
         data=f.read()
         if data!="":
             if "Results" in data:
-                FROMADDR = "trialsample7@gmail.com"
+                FROMADDR = "YOUR EMAIL ID"
                 LOGIN = FROMADDR
-                PASSWORD = "sample123"
-                TOADDRS = ["pavan0591@gmail.com"]
+                PASSWORD = "YOUR PASSWORD"
+                TOADDRS = ["DESTINATION EMAIL"]
                 SUBJECT = "Results have been announced . Please check!"
 
                 msg = ("From: %s\r\nTo: %s\r\nSubject: %s\r\n\r\n" % (FROMADDR, ", ".join(TOADDRS), SUBJECT))
-                msg += "\nAll the best . Haraye Namaha\r\n"
+                msg += "\nMESSAGE\r\n"
 
                 server = smtplib.SMTP('smtp.gmail.com', 587)
                 server.set_debuglevel(1)
@@ -30,12 +29,11 @@ def function1():
                 server.quit()
             #print "Mail has been sent."
     except:
-        print "Not yet out!"
+        print "ERROR!"
             
 today = datetime.date.today()
 sched = Scheduler()
 sched.start()
-day = today.day
-month = today.month
+#Scheduler which does the cron job
 sched.add_cron_job(function1, month='1', day='10', hour='10-15',minute='0,5,9,14,18,21,23,28,31,35,42,44,47,52,53,56,58')
     
